0581cb276d570b30556275c535df0d9502b5f7b0
cwmdavies <chris.davies@weavermanor.co.uk>
2021-04-15 14:36:18 +0100

    minor changes to the variables

--- Excel Manipulation/excel_manipulation.py
+++ Excel Manipulation/excel_manipulation.py
@@ -1,8 +1,9 @@
 from openpyxl import load_workbook, Workbook
 
 
-class _excel:
+class __excel:
     def __init__(self, name):
+        self.i = 0
         self.name = name
         filename = self.name + ".xlsx"
         workbook = Workbook()
@@ -11,16 +12,15 @@ class _excel:
         workbook = load_workbook(filename=self.name + ".xlsx")
         return workbook.sheetnames
     def add_columns(self, *col_name):
-        i = 0
         workbook = load_workbook(filename=self.name + ".xlsx")
         for value in col_name:
             if value not in workbook.sheetnames:
-                col_name = workbook.create_sheet(value, i)
-                i += 1
+                col_name = workbook.create_sheet(value, self.i)
+                self.i += 1
             else:
                 print(f"{value} already exists in {self.name}. Ignoring column creation!")
         if "Sheet" in workbook.sheetnames:
             del workbook["Sheet"]
         workbook.save(filename=self.name + ".xlsx")
 
-book1 = _excel("Test")
+book1 = __excel("Test")
