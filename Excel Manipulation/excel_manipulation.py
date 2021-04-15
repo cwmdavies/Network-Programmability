58f52dad65006170311d4c71e46527da3938ae6a
cwmdavies <chris.davies@weavermanor.co.uk>
2021-04-15 14:19:28 +0100

    Added methods into excel manipulation module

--- Excel Manipulation/excel_manipulation.py
+++ Excel Manipulation/excel_manipulation.py
@@ -7,12 +7,20 @@ class _excel:
         filename = self.name + ".xlsx"
         workbook = Workbook()
         workbook.save(filename=filename)
-    def get_sheet(self):
+    def get_sheets(self):
         workbook = load_workbook(filename=self.name + ".xlsx")
         return workbook.sheetnames
-    def add_col(self, col_name):
+    def add_columns(self, *col_name):
+        i = 0
         workbook = load_workbook(filename=self.name + ".xlsx")
-        col_name = workbook.create_sheet(col_name, 0)
+        for value in col_name:
+            if value not in workbook.sheetnames:
+                col_name = workbook.create_sheet(value, i)
+                i += 1
+            else:
+                print(f"{value} already exists in {self.name}. Ignoring column creation!")
+        if "Sheet" in workbook.sheetnames:
+            del workbook["Sheet"]
         workbook.save(filename=self.name + ".xlsx")
 
 book1 = _excel("Test")
