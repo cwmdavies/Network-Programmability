8a4d7cfe41db239dde29495dc4be2c3e005ba5ca
cwmdavies <chris.davies@weavermanor.co.uk>
2021-04-15 14:47:25 +0100

    Changes some variables

--- Excel Manipulation/excel_manipulation.py
+++ Excel Manipulation/excel_manipulation.py
@@ -5,14 +5,14 @@ class __excel:
     def __init__(self, name):
         self.i = 0
         self.name = name
-        filename = self.name + ".xlsx"
+        self.filename = self.name + ".xlsx"
         workbook = Workbook()
-        workbook.save(filename=filename)
+        workbook.save(filename=self.filename)
     def get_sheets(self):
-        workbook = load_workbook(filename=self.name + ".xlsx")
+        workbook = load_workbook(filename=self.filename)
         return workbook.sheetnames
     def add_columns(self, *col_name):
-        workbook = load_workbook(filename=self.name + ".xlsx")
+        workbook = load_workbook(filename=self.filename)
         for value in col_name:
             if value not in workbook.sheetnames:
                 col_name = workbook.create_sheet(value, self.i)
@@ -21,6 +21,6 @@ class __excel:
                 print(f"{value} already exists in {self.name}. Ignoring column creation!")
         if "Sheet" in workbook.sheetnames:
             del workbook["Sheet"]
-        workbook.save(filename=self.name + ".xlsx")
+        workbook.save(filename=self.filename)
 
 book1 = __excel("Test")
