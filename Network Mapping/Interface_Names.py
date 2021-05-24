2a146a30b5cae4c1e622b82b0f09c85732e1f06d
cwmdavies <chris.davies@weavermanor.co.uk>
2021-05-24 12:04:31 +0100

    Added script to retrieve the interface info

--- Network Mapping/Interface_Names.py
+++ Network Mapping/Interface_Names.py
@@ -13,11 +13,14 @@ import os
 import paramiko
 import datetime as time
 from openpyxl import load_workbook, Workbook
+import re
 
 jumpserver_private_addr = '10.251.6.31'   # The internal IP Address for the Jump server
 local_IP_addr = '127.0.0.1' # IP Address of the machine you are connecting from
 username = input("Please enter your username: ")
 password = input("Please enter your password: ")
+
+interfaces = dict()
 
 class excel_writer:
     def __init__(self, name):
@@ -82,6 +85,64 @@ def open_session(IP):
         error_log(f"Open Session Error: An unknown error occured for IP: {IP}!")
         return None, None, False
 
+def get_interfaces(IP):
+    interface_names = list()
+    ssh, jumpbox, connection = open_session(IP)
+    if not connection:
+        return None
+    try:
+        stdin, stdout, stderr = ssh.exec_command("show ip interface brief")
+        stdout = stdout.read()
+        stdout = stdout.decode("utf-8")
+        regex = r"^(\b(Ten|Gig|Loo|Vla|Fas|Twe|Ten|Fo).{20})"
+        matches = re.finditer(regex, stdout, re.MULTILINE)
+        for match in matches:
+            temp_interface_name = match.group(1)
+            temp_interface_name = temp_interface_name.strip()
+            interface_names.append(temp_interface_name)
+        return interface_names
+    except paramiko.ssh_exception.AuthenticationException:
+        error_log(f"Interfaces function Error: Authentication to IP: {IP} failed! Please check your IP, username and password.")
+        return None
+    except paramiko.ssh_exception.NoValidConnectionsError:
+        error_log(f"Interfaces function Error: Unable to connect to IP: {IP}!")
+        return None
+    except (ConnectionError, TimeoutError):
+        error_log(f"Interfaces function Error: Timeout error occured for IP: {IP}!")
+        return None
+    except:
+        error_log(f"Interfaces function Error: An unknown error occured for IP: {IP}!")
+        return None
+    finally:
+        ssh.close()
+        jumpbox.close()
+
+def get_inte_descr(IP, int_name):
+    command = f"show run interface {int_name} | inc description"
+    ssh, jumpbox, connection = open_session(IP)
+    if not connection:
+        return None
+    try:
+        stdin, stdout, stderr = ssh.exec_command(command)
+        stdout = stdout.read()
+        stdout = stdout.decode("utf-8")
+        Inter_Desc = re.search(".*description.*", stdout)
+        return Inter_Desc[0]
+    except paramiko.ssh_exception.SSHException:
+        error_log(f"Interface description Function Error: There is an error connecting or establishing SSH session to IP Address {IP}")
+    except:
+        error_log(f"Interface description Function Error: An unknown error occured for IP: {IP}!")
+        return None, False
+    finally:
+        ssh.close()
+        jumpbox.close()
+
+
+#############################################################################################################################################
+##          Logging Functions
+#
+
+
 def error_log(message, i=0):
     dateTimeObj = time.datetime.now()
     datetime = dateTimeObj.strftime("%d/%m/%Y %H:%M:%S")
@@ -102,25 +163,6 @@ def output_log(message, i=0):
     if i == 1:
         print(message)
 
-def interface_descr(IP, int_name):
-    command = f"show run interface {int_name} | inc description"
-    ssh, jumpbox, connection = open_session(IP)
-    if not connection:
-        return None
-    try:
-        output_log("Getting Interface name")
-        stdin, stdout, stderr = ssh.exec_command(command)
-        stdout = stdout.read()
-        stdout = stdout.decode("utf-8")
-        int_description = stdout
-        ssh.close()
-        jumpbox.close()
-        return int_description
-    except paramiko.ssh_exception.SSHException:
-        error_log(f"Interface description Function Error: There is an error connecting or establishing SSH session to IP Address {IP}")
-    except:
-        error_log(f"Interface description Function Error: An unknown error occured for IP: {IP}!")
-        return None, False
-    finally:
-        ssh.close()
-        jumpbox.close()
+#
+##
+#############################################################################################################################################
