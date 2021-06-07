f5d65bd62a8e394a228145d7ef4231996793e0d8
cwmdavies <chris.davies@weavermanor.co.uk>
2021-06-07 13:13:20 +0100

    Testing Logger code

--- Logging/logging_code.py
+++ Logging/logging_code.py
@@ -6,17 +6,20 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Create handlers
+info_handler = logging.StreamHandler()
 debug_handler = logging.FileHandler('debug.log')
 debug_handler.setLevel(logging.DEBUG)
-
+info_handler.setLevel(logging.INFO)
 
 # Create formatters and add it to handlers
 debug_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+info_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 debug_handler.setFormatter(debug_format)
+info_handler.setFormatter(info_format)
 
 # Add handlers to the logger
 logger.addHandler(debug_handler)
-
+logger.addHandler(info_handler)
 
 # Logger commands
 logger.debug('This is a debug message')
