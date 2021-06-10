f54ae5d99bab70afeab6fae7b5ef49fb9ce7a4e0
cwmdavies <chris.davies@weavermanor.co.uk>
2021-06-10 11:59:20 +0100

    Testing Logger code

--- Logging/logging_code.py
+++ Logging/logging_code.py
@@ -22,3 +22,11 @@ logging.basicConfig(
 
 # Define your own logger name
 log = logging.getLogger(__name__)
+
+''' Logger commands
+log.debug('This is a debug message')
+log.info('This is an info message')
+log.warning('This is a warning message')
+log.error('This is an error message')
+log.critical('This is a critical message')
+'''
