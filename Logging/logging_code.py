52b8a361a535ef6ff5fd3a95cb1c5b4e474320a9
cwmdavies <chris.davies@weavermanor.co.uk>
2021-06-10 11:51:46 +0100

    Testing Logger code

--- Logging/logging_code.py
+++ Logging/logging_code.py
@@ -1,29 +1,24 @@
 import logging
+import sys
 
-logging.basicConfig(level=logging.DEBUG)
+# Log file location
+logfile = 'debug.log'
+# Define the log format
+log_format = (
+    '[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s')
 
-# Create a custom logger
-logger = logging.getLogger(__name__)
+# Define basic configuration
+logging.basicConfig(
+    # Define logging level
+    level=logging.DEBUG,
+    # Declare the object we created to format the log messages
+    format=log_format,
+    # Declare handlers
+    handlers=[
+        logging.FileHandler(logfile),
+        logging.StreamHandler(sys.stdout),
+    ]
+)
 
-# Create handlers
-info_handler = logging.StreamHandler()
-debug_handler = logging.FileHandler('debug.log')
-debug_handler.setLevel(logging.DEBUG)
-info_handler.setLevel(logging.INFO)
-
-# Create formatters and add it to handlers
-debug_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-info_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-debug_handler.setFormatter(debug_format)
-info_handler.setFormatter(info_format)
-
-# Add handlers to the logger
-logger.addHandler(debug_handler)
-logger.addHandler(info_handler)
-
-# Logger commands
-logger.debug('This is a debug message')
-logger.info('This is an info message')
-logger.warning('This is a warning message')
-logger.error('This is an error message')
-logger.critical('This is a critical message')
+# Define your own logger name
+log = logging.getLogger(__name__)
