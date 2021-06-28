cae1476b27609dd3a99699173f69f56aef5002a2
cwmdavies <chris.davies@weavermanor.co.uk>
2021-06-28 14:54:07 +0100

    TEXTFSM Testing

--- TEXTFSM/cdpinfo.py
+++ TEXTFSM/cdpinfo.py
@@ -1,13 +1,53 @@
-import sys
+###############################################
+#            Under Construction               #
+#                Design Phase                 #
+#                                             #
+###############################################
+
+import paramiko
 import textfsm
-from tabulate import tabulate
+from getpass import getpass
 
-template = sys.argv[1]
-output_file = sys.argv[2]
+IPAddr = input("Enter an IP Address")
+username = input("Enter your username: ")
+password = getpass("Enter your Password: ")
+jump_server_address = '10.251.6.31'   # The internal ip Address for the Jump server
+local_IP_address = '127.0.0.1'  # ip Address of the machine you are connecting from
 
-with open(template) as f, open(output_file) as output:
-    re_table = textfsm.TextFSM(f)
-    header = re_table.header
-    result = re_table.ParseText(output.read())
-    print(result)
-    print(tabulate(result, headers=header))
+
+def jump_session(ip):
+    jump_box = paramiko.SSHClient()
+    jump_box.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    jump_box.connect(jump_server_address, username=username, password=password)
+    jump_box_transport = jump_box.get_transport()
+    src_address = (local_IP_address, 22)
+    destination_address = (ip, 22)
+    jump_box_channel = jump_box_transport.open_channel("direct-tcpip", destination_address, src_address)
+    target = paramiko.SSHClient()
+    target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    target.connect(destination_address, username=username, password=password, sock=jump_box_channel)
+    return target, jump_box, True
+
+
+def get_cdp_details(ip):
+    ssh, jump_box, connection = jump_session(ip)
+    if not connection:
+        return None
+    _, stdout, _ = ssh.exec_command("show cdp neighbors detail")
+    stdout = stdout.read()
+    stdout = stdout.decode("utf-8")
+    with open("template.txt") as f:
+        re_table = textfsm.TextFSM(f)
+        result = re_table.ParseText(stdout)
+    ssh.close()
+    jump_box.close()
+    return result
+
+
+def main():
+    cdp_details = get_cdp_details(IPAddr)
+    print(cdp_details)
+
+
+if __name__ == "__main__":
+    main()
