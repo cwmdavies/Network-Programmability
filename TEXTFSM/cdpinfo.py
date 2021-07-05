5f9e90dbb60c46c050305fbe2a68d5d1ca52f5f7
cwmdavies <chris.davies@weavermanor.co.uk>
2021-07-05 15:57:23 +0100

    text fsm script changes

--- TEXTFSM/cdpinfo.py
+++ TEXTFSM/cdpinfo.py
@@ -37,7 +37,7 @@ def get_cdp_details(ip):
     _, stdout, _ = ssh.exec_command("show cdp neighbors detail")
     stdout = stdout.read()
     stdout = stdout.decode("utf-8")
-    with open("template.txt") as f:
+    with open("TEXTFSM_TEMPLATES/cdp_details.txt") as f:
         re_table = textfsm.TextFSM(f)
         result = re_table.ParseText(stdout)
     collection_of_results = [dict(zip(re_table.header, entry)) for entry in result]
@@ -46,7 +46,24 @@ def get_cdp_details(ip):
     return collection_of_results
 
 
+def get_hostname(ip):
+    ssh, jump_box, connection = jump_session(ip)
+    if not connection:
+        return None
+    _, stdout, _ = ssh.exec_command("show run | inc hostname")
+    stdout = stdout.read()
+    stdout = stdout.decode("utf-8")
+    with open("TEXTFSM_TEMPLATES/hostname.txt") as f:
+        re_table = textfsm.TextFSM(f)
+        result = re_table.ParseText(stdout)
+        hostname = result[0][0]
+    ssh.close()
+    jump_box.close()
+    return hostname
+
+
 def main():
+    hostname = get_hostname(IPAddr)
     cdp_details = get_cdp_details(IPAddr)
 
     filename = "CDP_Neighbors_Detail.xlsx"
@@ -56,30 +73,33 @@ def main():
     workbook.create_sheet("CDP Neighbors Detail")
     del workbook["Sheet"]
     ws = workbook["CDP Neighbors Detail"]
-    ws["A1"] = "DESTINATION_HOST"
-    ws["B1"] = "MANAGEMENT_IP"
-    ws["C1"] = "PLATFORM"
+    ws["A1"] = "LOCAL_HOST"
+    ws["B1"] = "LOCAL_PORT"
+    ws["C1"] = "REMOTE_HOST"
     ws["D1"] = "REMOTE_PORT"
-    ws["E1"] = "LOCAL_PORT"
-    ws["F1"] = "SOFTWARE_VERSION"
-    ws["G1"] = "CAPABILITIES"
-    ws.column_dimensions['A'].width = "42"
+    ws["E1"] = "REMOTE_IP"
+    ws["F1"] = "PLATFORM"
+    ws["G1"] = "SOFTWARE_VERSION"
+    ws["H1"] = "CAPABILITIES"
+    ws.column_dimensions['A'].width = "22"
     ws.column_dimensions['B'].width = "22"
     ws.column_dimensions['C'].width = "42"
     ws.column_dimensions['D'].width = "22"
-    ws.column_dimensions['E'].width = "25"
-    ws.column_dimensions['F'].width = "120"
-    ws.column_dimensions['G'].width = "42"
+    ws.column_dimensions['E'].width = "22"
+    ws.column_dimensions['F'].width = "42"
+    ws.column_dimensions['G'].width = "120"
+    ws.column_dimensions['H'].width = "42"
 
     index = 2
     for entry in cdp_details:
-        ws[f"A{index}"] = entry["DESTINATION_HOST"]
-        ws[f"B{index}"] = entry["MANAGEMENT_IP"]
-        ws[f"C{index}"] = entry["PLATFORM"]
+        ws[f"A{index}"] = hostname
+        ws[f"B{index}"] = entry["LOCAL_PORT"]
+        ws[f"C{index}"] = entry["REMOTE_HOST"]
         ws[f"D{index}"] = entry["REMOTE_PORT"]
-        ws[f"E{index}"] = entry["LOCAL_PORT"]
-        ws[f"F{index}"] = entry["SOFTWARE_VERSION"]
-        ws[f"G{index}"] = entry["CAPABILITIES"]
+        ws[f"E{index}"] = entry["REMOTE_IP"]
+        ws[f"F{index}"] = entry["PLATFORM"]
+        ws[f"G{index}"] = entry["SOFTWARE_VERSION"]
+        ws[f"H{index}"] = entry["CAPABILITIES"]
         workbook.save(filename=filename)
         index += 1
     workbook.save(filename=filename)
