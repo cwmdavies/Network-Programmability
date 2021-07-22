1ce512fcd45f26ca0bfe3ac639b095bcff8a65db
cwmdavies <chris.davies@weavermanor.co.uk>
2021-07-22 11:02:43 +0100

    text fsm script changes

--- TEXTFSM/cdpinfo.py
+++ TEXTFSM/cdpinfo.py
@@ -9,16 +9,68 @@ import textfsm
 from getpass import getpass
 from openpyxl import load_workbook, Workbook
 import ipaddress
+import logging
+import sys
 
+Debugging = 0
 IPAddr = input("Enter an IP Address: ")
 username = input("Enter your username: ")
 password = getpass("Enter your Password: ")
 jump_server_address = '10.251.6.31'  # The internal ip Address for the Jump server
 local_IP_address = '127.0.0.1'  # ip Address of the machine you are connecting from
 IP_LIST = []
+Hostnames_List = []
 collection_of_results = []
 filename = "CDP_Neighbors_Detail.xlsx"
 index = 2
+
+# ---------------------------------------------------------
+# -------------- Logging Configuration Start --------------
+
+# Log file location
+logfile = 'debug.log'
+# Define the log format
+log_format = (
+    '[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s')
+
+# Define basic configuration
+if Debugging == 0:
+    logging.basicConfig(
+        # Define logging level
+        level=logging.INFO,
+        # Declare the object we created to format the log messages
+        format=log_format,
+        # Declare handlers
+        handlers=[
+            logging.FileHandler(logfile),
+            logging.StreamHandler(sys.stdout),
+        ]
+    )
+elif Debugging == 1:
+    logging.basicConfig(
+        # Define logging level
+        level=logging.DEBUG,
+        # Declare the object we created to format the log messages
+        format=log_format,
+        # Declare handlers
+        handlers=[
+            logging.FileHandler(logfile),
+            logging.StreamHandler(sys.stdout),
+        ]
+    )
+
+# Define your own logger name
+log = logging.getLogger(__name__)
+
+''' Logger commands
+log.debug('This is a debug message')
+log.info('This is an info message')
+log.warning('This is a warning message')
+log.error('This is an error message')
+log.critical('This is a critical message')
+'''
+# --------------- Logging Configuration End ---------------
+# ---------------------------------------------------------
 
 
 def ip_check(ip):
@@ -29,13 +81,13 @@ def ip_check(ip):
         return False
 
 
-# noinspection PyBroadException,PyTypeChecker
 def jump_session(ip):
     if not ip_check(ip):
-        print(f"open_session function error: "
-              f"ip Address {ip} is not a valid Address. Please check and restart the script!", )
-        return None, None, False
+        log.error(f"open_session function error: "
+                  f"ip Address {ip} is not a valid Address. Please check and restart the script!",)
+        return None, False
     try:
+        log.info(f"Trying to establish a connection to: {ip}")
         jump_box = paramiko.SSHClient()
         jump_box.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         jump_box.connect(jump_server_address, username=username, password=password)
@@ -46,32 +98,44 @@ def jump_session(ip):
         target = paramiko.SSHClient()
         target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         target.connect(destination_address, username=username, password=password, sock=jump_box_channel)
-    except Exception as err:
-        print(f"Unable to connect to IP Address: {ip}")
+        log.info(f"Connection to ip: {ip} established")
+        return target, jump_box, True
+    except paramiko.ssh_exception.AuthenticationException:
+        log.error(f"Authentication to ip: {ip} failed! Please check your ip, username and password.")
         return None, None, False
-    return target, jump_box, True
+    except paramiko.ssh_exception.NoValidConnectionsError:
+        log.error(f"Unable to connect to ip: {ip}!")
+        return None, None, False
+    except (ConnectionError, TimeoutError):
+        log.error(f"Timeout error occurred for ip: {ip}!")
+        return None, None, False
+    except Exception as err:
+        log.error(f"Open Session Error: An unknown error occurred for ip: {ip}!")
+        log.error(f"\t Error: {err}")
+        return None, None, False
 
 
 def get_cdp_details(ip):
-    hostname = get_hostname(ip)
     ssh, jump_box, connection = jump_session(ip)
     if not connection:
         return None
-    _, stdout, _ = ssh.exec_command("show cdp neighbors detail")
-    stdout = stdout.read()
-    stdout = stdout.decode("utf-8")
-    with open("TEXTFSM_TEMPLATES/cdp_details.txt") as f:
-        re_table = textfsm.TextFSM(f)
-        result = re_table.ParseText(stdout)
-    result = [dict(zip(re_table.header, entry)) for entry in result]
-    for entry in result:
-        entry['LOCAL_HOST'] = hostname
-        entry['LOCAL_IP'] = ip
-        collection_of_results.append(entry)
-
-        if entry["REMOTE_IP"] not in IP_LIST:
-            if 'Switch' in entry['CAPABILITIES']:
-                IP_LIST.append(entry["REMOTE_IP"])
+    hostname = get_hostname(ip)
+    if hostname not in Hostnames_List:
+        Hostnames_List.append(hostname)
+        _, stdout, _ = ssh.exec_command("show cdp neighbors detail")
+        stdout = stdout.read()
+        stdout = stdout.decode("utf-8")
+        with open("TEXTFSM_TEMPLATES/cdp_details.txt") as f:
+            re_table = textfsm.TextFSM(f)
+            result = re_table.ParseText(stdout)
+        result = [dict(zip(re_table.header, entry)) for entry in result]
+        for entry in result:
+            entry['LOCAL_HOST'] = hostname
+            entry['LOCAL_IP'] = ip
+            collection_of_results.append(entry)
+            if entry["REMOTE_IP"] not in IP_LIST:
+                if 'Switch' in entry['CAPABILITIES']:
+                    IP_LIST.append(entry["REMOTE_IP"])
     ssh.close()
     jump_box.close()
 
@@ -105,34 +169,37 @@ def to_excel(cdp_details):
     ws = workbook["CDP Neighbors Detail"]
     ws["A1"] = "LOCAL_HOST"
     ws["B1"] = "LOCAL_PORT"
-    ws["C1"] = "REMOTE_HOST"
-    ws["D1"] = "REMOTE_PORT"
-    ws["E1"] = "REMOTE_IP"
-    ws["F1"] = "PLATFORM"
-    ws["G1"] = "SOFTWARE_VERSION"
-    ws["H1"] = "CAPABILITIES"
+    ws["C1"] = "LOCAL_IP"
+    ws["D1"] = "REMOTE_HOST"
+    ws["E1"] = "REMOTE_PORT"
+    ws["F1"] = "REMOTE_IP"
+    ws["G1"] = "PLATFORM"
+    ws["H1"] = "SOFTWARE_VERSION"
+    ws["I1"] = "CAPABILITIES"
     ws.column_dimensions['A'].width = "25"
     ws.column_dimensions['B'].width = "25"
-    ws.column_dimensions['C'].width = "45"
-    ws.column_dimensions['D'].width = "25"
+    ws.column_dimensions['C'].width = "25"
+    ws.column_dimensions['D'].width = "45"
     ws.column_dimensions['E'].width = "25"
     ws.column_dimensions['F'].width = "25"
-    ws.column_dimensions['G'].width = "120"
-    ws.column_dimensions['H'].width = "45"
+    ws.column_dimensions['G'].width = "25"
+    ws.column_dimensions['H'].width = "120"
+    ws.column_dimensions['I'].width = "45"
+    ws.auto_filter.ref = ws.dimensions
     workbook.save(filename=filename)
     try:
         for entry in cdp_details:
             ws[f"A{index}"] = entry["LOCAL_HOST"]
             ws[f"B{index}"] = entry["LOCAL_PORT"]
-            ws[f"C{index}"] = entry["REMOTE_HOST"]
-            ws[f"D{index}"] = entry["REMOTE_PORT"]
-            ws[f"E{index}"] = entry["REMOTE_IP"]
-            ws[f"F{index}"] = entry["PLATFORM"]
-            ws[f"G{index}"] = entry["SOFTWARE_VERSION"]
-            ws[f"H{index}"] = entry["CAPABILITIES"]
+            ws[f"C{index}"] = entry["LOCAL_IP"]
+            ws[f"D{index}"] = entry["REMOTE_HOST"]
+            ws[f"E{index}"] = entry["REMOTE_PORT"]
+            ws[f"F{index}"] = entry["REMOTE_IP"]
+            ws[f"G{index}"] = entry["PLATFORM"]
+            ws[f"H{index}"] = entry["SOFTWARE_VERSION"]
+            ws[f"I{index}"] = entry["CAPABILITIES"]
             workbook.save(filename=filename)
             index += 1
-
     except Exception as err:
         print("An Exception Occurred")
         print({err})
