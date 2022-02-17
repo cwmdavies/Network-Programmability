20a918890f1032868d2dca95a997ca170b25aa63
cwmdavies <chris.davies@weavermanor.co.uk>
2022-02-17 14:28:56 +0000

    Diagram testing

--- Network Mapping/Archive/Diagram/main.py
+++ Network Mapping/Archive/Diagram/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel(r'CDP_Neighbors_Detail.xlsx')
 
-G = nx.from_pandas_edgelist(df, 'LOCAL_HOST', 'REMOTE_HOST')
+G = nx.from_pandas_edgelist(df, 'LOCAL_HOST', 'DESTINATION_HOST')
 nx.draw_planar(G, with_labels=True, node_color="skyblue", node_shape="o", alpha=0.5, linewidths=4, font_color="grey",
                font_weight="bold", width=2, edge_color="grey", horizontalalignment="left", verticalalignment="top")
 
